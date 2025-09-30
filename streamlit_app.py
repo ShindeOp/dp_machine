10f2f0da8a5bd4b6b3f7f104fb1c48a03bdbc0b1
Abhinav Shinde <shindeabhinav67@gmail.com>
2025-09-30 17:23:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,16 @@ import streamlit as st
 import pandas as pd
 
 st.title('🎈 ML app')
+st.info('This is an ML app')
 
-st.info('This is a ml app')
+# Raw URL to the CSV
+csv_url = "https://raw.githubusercontent.com/ShindeOp/dp_machine/master/data.csv"
 
-df=pd.read_csv('https://raw.githubusercontent.com/ShindeOp/dp_machine/master/data.csv')
-
-
+try:
+    # Attempt to load the CSV file
+    df = pd.read_csv(csv_url)
+    st.success("Dataset loaded successfully!")
+    st.dataframe(df)  # Display dataframe
+except Exception as e:
+    # If an error occurs, display it in Streamlit
+    st.error(f"Error loading dataset: {e}")
